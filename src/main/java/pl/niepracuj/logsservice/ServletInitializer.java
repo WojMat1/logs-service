59accd274864bc84d9d4571167f801d5d16616c1
miyopg <miyo001@gmail.com>
2022-10-15 13:08:06 +0200

    Inital Commit

---
+++ src/main/java/pl/niepracuj/logsservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.niepracuj.logsservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LogsServiceApplication.class);
+    }
+
+}
